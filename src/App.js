92bf0473adb0a937c1e051dc9a93cdf38937f1bb
khademishoja <khademishoja@gmail.com>
2022-11-14 16:27:42 +0100

    Setup router, basic routes and placeholder components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ShopPage from "./pages/ShopPage";
+import DetailsPage from "./pages/DetailsPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/details/:id" element={<DetailsPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ShopPage.js
@@ -0,0 +1,8 @@
+const ShopPage = () => {
+  return (
+    <div>
+      <h1>Shope Page</h1>
+    </div>
+  );
+};
+export default ShopPage;
