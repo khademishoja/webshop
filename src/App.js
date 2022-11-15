a712c22a60c9000ac9290f30c9d252ce144e665c
khademishoja <khademishoja@gmail.com>
2022-11-15 15:14:02 +0100

    changed style page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
 import DetailsPage from "./pages/DetailsPage";
+import { Navbar } from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
         <Route path="/details/:id" element={<DetailsPage />} />
       </Routes>
