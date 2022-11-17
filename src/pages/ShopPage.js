5ba3d7c25b132e831d4807f5ebf6475c56eecaba
gilbertjoalex <alexandre@joaris.net>
2022-11-17 03:22:46 +0100

    shop-page update-button style resolve

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,8 +1,66 @@
-const ShopPage = () => {
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import ProductBlock from "../components/ProductBlock";
+
+// function compare_name(patient_a, patient_b) {
+//   return patient_a.lastName.localeCompare(patient_b.lastName);
+// }
+
+function ProductsPage() {
+  const [products, setProducts] = useState("");
+
+  useEffect(() => {
+    async function getProducts() {
+      const productResponse = await axios.get("http://localhost:4000/products");
+      setProducts(productResponse.data);
+    }
+    getProducts();
+  }, []);
+
+  // const patients_sorted = [...patients].sort(compare_name);
+
+  console.log(products, "sorted");
   return (
     <div>
-      <h1>Shope Page</h1>
+      <h1>All Products</h1>
+      {products ? (
+        products.map((patientObj) => (
+          <ProductBlock
+            title={patientObj.title}
+            price={patientObj.price}
+            description={patientObj.description}
+            rating={patientObj.rating}
+            mainImage={patientObj.mainImage}
+            createdAt={patientObj.createdAt}
+            updatedAt={patientObj.updatedAt}
+            categoryId={patientObj.categoryId}
+          />
+        ))
+      ) : (
+        <p>Loading ..</p>
+      )}
     </div>
   );
-};
-export default ShopPage;
+}
+// <div>
+//   <ProductBlock />
+// </div>
+
+//   {products ? (
+//     products.map((patientObj) => (
+
+//       //   firstName={patientObj.firstName}
+//       //   lastName={patientObj.lastName}
+//       //   PatientId={patientObj.id}
+//       //   Dob={patientObj.dateOfBirth}
+//       // />
+//     ))
+//   ) : (
+//     <p>Loading ..</p>
+//   )}
+// </div>
+//   );
+// }
+
+export default ProductsPage;
